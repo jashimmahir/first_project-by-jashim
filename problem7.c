4e79dc6798a397171845a06b4c7af89d02e85353
jashimmahir <mdjoshimuddin542@gmail.com>
2025-09-06 17:06:08 +0600

    Add files via upload

---
+++ problem7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    FILE *im=fopen("img01.jpg","rb");
+    if(im==NULL)
+    {
+        printf("can not read");
+        return 1;
+    }
+    int x=fseek(im,0,SEEK_END);
+    if(x!=0)
+    {
+        printf("can not fseek work");
+        return 1;
+    }
+    long long int c= (ftell(im))/1024.0;
+    printf("%lld mb",c);
+    fclose(im);
+    return 0;
+
+
+}
